c8ec4db1684a408fc315394c899f350f6f867165
Ryan Hinchcliff <rhinch303@gmail.com>
2026-03-03 11:58:44 -0600

    decoder ring block

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // This ensures the decoder ring is destroyed
+  }
 })
