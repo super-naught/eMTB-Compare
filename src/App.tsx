f54836d1bd3d3e13548e026ba4a48745728e0859
Ryan Hinchcliff <rhinch303@gmail.com>
2026-03-05 14:26:46 -0600

    update test

--- src/App.tsx
+++ src/App.tsx
@@ -554,7 +554,7 @@ const toggleFavorite = async (buildId: string) => {
                             </div>
                             <h1 className="text-4xl sm:text-5xl lg:text-6xl font-black uppercase tracking-tighter text-white leading-[1.05]">
                               FIND YOUR <br />
-                              <span className="text-blue-500 mt-1 inline-block">Super Duper Bike!</span> 
+                              <span className="text-blue-500 mt-1 inline-block">DREAM BIKE</span> 
                             </h1>
                           </div>
                           <p className="text-sm sm:text-base text-slate-400 max-w-lg leading-relaxed font-medium">
